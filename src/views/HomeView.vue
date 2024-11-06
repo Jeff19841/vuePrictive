56949d872b7a04ce1c255d03094b6d375d77feef
unknown <F131329778@pwg.mil.tw>
2024-11-06 16:33:24 +0800

    初始化完成

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <section
+    class="flex w-full h-full flex-col items-center justify-center p-[300px] gap-y-10"
+  >
+    <router-link
+      to="/TodoListView"
+      class="font-bold text-2xl underline hover:text-blue-600"
+      >TodoListView</router-link
+    >
+  </section>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ src/views/TodoListView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>這裡是TodoListView</div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
