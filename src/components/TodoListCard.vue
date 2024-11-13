b615106b35ee0dee1fc3891a3f071ec9700accad
unknown <F131329778@pwg.mil.tw>
2024-11-13 11:27:43 +0800

    第一次完成

--- src/components/TodoListCard.vue
+++ src/components/TodoListCard.vue
@@ -1,20 +1,90 @@
 <template>
   <div
-    class="flex h-[200px] w-[48%] flex-col justify-between rounded-xl border-4 border-stone-700 bg-slate-300 px-2.5 py-1.5 shadow-md"
+    class="group flex h-[200px] w-[48%] flex-col justify-start gap-y-4 rounded-xl border-4 border-stone-700 bg-slate-300 px-2 py-1.5 shadow-md"
   >
     <!-- ?代表有可能還不存在 -->
-    <div class="mb-2 px-5 text-center text-2xl font-bold">
-      {{ props?.todoListItem?.title || "XX" }}
+    <div class="flex justify-between overflow-hidden px-5 text-2xl font-bold">
+      <!--勾選與標題 -->
+      <div class="flex max-w-[60%] gap-3">
+        <!-- 勾選 -->
+        <label class="pt-1" v-if="listItemData.isDisplay != true">
+          <!-- v-model -->
+          <input
+            v-model="listItemData.isClick"
+            class="h-6 w-6 rounded-2xl"
+            type="checkbox"
+          />
+        </label>
+        <!-- 標題 -->
+        <div class="line-clamp-1">
+          {{ listItemData?.title || "XX" }}
+        </div>
+      </div>
+      <!-- 編輯鈕和刪除鈕 -->
+      <div
+        v-if="listItemData.isDisplay != true"
+        class="hidden shrink-0 gap-4 group-hover:flex"
+      >
+        <!-- 編輯鈕 -->
+        <button @click="$emit('editCard', 1)">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+            />
+          </svg>
+        </button>
+        <button @click="$emit('deleteEditingCard', 1)">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
-    <div class="mb-2 flex-1 overflow-hidden text-center">
-      {{ props?.todoListItem?.content || "???" }}
+    <!-- 內容 -->
+    <div class="mb-2 line-clamp-4 overflow-hidden break-words px-5 text-start">
+      {{ listItemData?.content || "???" }}
     </div>
-    <div class="text-right">顯示更多</div>
   </div>
 </template>
 
 <script setup>
-const props = defineProps(["todoListItem"]);
+const { isShowEditIcon, listItemData } = defineProps({
+  isShowEditIcon: {
+    type: Boolean,
+    default: false,
+  },
+  listItemData: {
+    type: Object,
+  },
+  componentType: {
+    type: String,
+    default: "edit", // edit, display
+  },
+  clickedCheckbox: {
+    type: Boolean,
+    default: false,
+  },
+});
 </script>
 
 <style scoped></style>
--- src/views/TodoListView.vue
+++ src/views/TodoListView.vue
@@ -5,40 +5,68 @@
       <!-- 卡片群組編輯區 -->
       <section class="flex-1 rounded-2xl bg-green-800 p-12">
         <h1 class="my-8 text-4xl font-bold text-white">卡片群組編輯區</h1>
-        <div class="flex justify-end">
-          <!-- 新增卡片鈕 -->
-          <button
-            v-if="pageStatus.mode != 'normal'"
-            class="mb-3 w-32 rounded-md bg-gray-300 p-2"
-            @click="pageStatus.mode = 'normal'"
-          >
-            取消
-          </button>
-          <!-- 新增卡片鈕 -->
-          <button
-            v-else
-            class="mb-3 w-32 rounded-md bg-gray-300 p-2"
-            @click="pageStatus.mode = 'creating'"
-          >
-            進入新增模式
-          </button>
+        <!-- TODO全選/反選 -->
+        <div class="flex justify-between">
+          <div class="">
+            <button
+              @click="pageAction.selectAll"
+              class="mb-3 mr-3 w-16 rounded-md bg-gray-300 p-2"
+            >
+              全選
+            </button>
+            <button
+              @click="pageAction.selectReAll"
+              class="mb-3 w-16 rounded-md bg-gray-300 p-2"
+            >
+              反選
+            </button>
+          </div>
+          <!-- 新增mode鈕 -->
+          <div class="flex justify-end">
+            <button
+              v-if="pageStatus.mode !== 'normal'"
+              class="mb-3 w-32 rounded-md bg-gray-300 p-2"
+              @click="pageAction.clearCard"
+            >
+              取消
+            </button>
+            <button
+              v-else
+              class="mb-3 w-32 rounded-md bg-gray-300 p-2"
+              @click="pageStatus.mode = 'creating'"
+            >
+              進入新增模式
+            </button>
+          </div>
         </div>
-        <!-- box -->
+        <!-- 卡片區 -->
         <div class="flex flex-wrap gap-5">
           <TodoListCard
-            v-for="(todoListItem, key) in todoListItemsData"
-            :todoListItem="todoListItem"
+            v-for="(todoListItem, key) in pageData.editItemList"
+            :listItemData="todoListItem"
             :key="key"
-            @click="goToEditing(key)"
-          ></TodoListCard>
-          <button class="rounded-md bg-gray-300 p-2">確定上傳本區卡片</button>
-          <button class="rounded-md bg-gray-300 p-2">取消</button>
+            @editCard="pageAction.goToEditing(key)"
+            @deleteEditingCard="pageAction.deleteCard(key)"
+          >
+          </TodoListCard>
+        </div>
+        <!-- 卡片編輯區確定上傳/取消按鈕 -->
+        <div class="flex justify-end gap-5">
+          <button
+            class="flex rounded-md bg-gray-300 p-2"
+            @click="pageAction.renewDisplayItemList"
+          >
+            確定上傳本區卡片
+          </button>
+          <!-- <button class="rounded-md bg-gray-300 p-2">
+            取消
+          </button> -->
         </div>
       </section>
       <!-- 單張卡片編輯區 -->
       <section class="flex-1 rounded-2xl bg-green-800 p-12">
         <h1 class="my-8 text-4xl font-bold text-white">
-          編輯區{{ pageStatus.mode }}
+          編輯區{{ pageStatus.showUpCreatingMode }}
         </h1>
         <!-- 編輯區 -->
         <div
@@ -55,7 +83,8 @@
                 </h2>
                 <input
                   type="text"
-                  class="flex-1 rounded-lg bg-green-200 bg-transparent px-2 focus:outline-none"
+                  class="flex-1 rounded-lg bg-green-200 px-2 focus:outline-none"
+                  v-model="pageData.inputData.title"
                 />
               </div>
               <!-- 內容 -->
@@ -64,20 +93,24 @@
                   內容:
                 </h2>
                 <textarea
-                  class="h-[200px] flex-1 rounded-lg bg-green-200 bg-transparent px-2 focus:outline-none"
+                  v-model="pageData.inputData.content"
+                  class="h-[200px] flex-1 rounded-lg bg-green-200 px-2 focus:outline-none"
                 ></textarea>
               </div>
             </div>
             <!-- 編輯區按鈕 -->
             <div class="flex justify-end">
               <button
-                @click="pageStatus.mode = 'normal'"
+                @click="clearCard"
                 class="mr-3 rounded-md bg-green-200 p-2 font-bold"
               >
                 取消
               </button>
-              <button class="rounded-md bg-green-800 p-2 font-bold text-white">
-                新增
+              <button
+                class="rounded-md bg-green-800 p-2 font-bold text-white"
+                @click="pageAction.finishEditingOrCreating"
+              >
+                完成
               </button>
             </div>
           </div>
@@ -90,8 +123,9 @@
           <h1 class="my-8 text-4xl font-bold text-white">顯示區</h1>
           <div class="flex flex-wrap gap-5">
             <TodoListCard
-              v-for="todoListItem in todoListItemsData"
-              :todoListItem="todoListItem"
+              v-for="(showListItem, key) in pageData.displayItemList"
+              :listItemData="showListItem"
+              :key="key"
             ></TodoListCard>
           </div>
         </section>
@@ -101,52 +135,153 @@
 </template>
 
 <script setup>
-import { reactive } from "vue";
+import { computed, reactive, ref } from "vue";
 import TodoListCard from "@/components/TodoListCard.vue";
-
-const todoListItemsData = reactive([
+// 來自資料庫的資料(不能更動)
+const todoListItemsData = [
   {
-    title: "動作",
+    id: 1,
+    title: "動作 ",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et beatae eaque odio suscipit eos consectetur, molestiae unde totam a sint cumque libero? Autem quae laboriosam enim similique exercitationem eaque iusto.",
   },
   {
+    id: 2,
     title: "黑幫",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et beatae eaque odio suscipit eos consectetur, molestiae unde totam a sint cumque libero? Autem quae laboriosam enim similique exercitationem ",
   },
   {
+    id: 3,
     title: "動畫",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et beatae eaque odio suscipit eos consectetur, molestiae unde totam a sint cumque libero? Autem quae laboriosam enim similique exercitationem eaq",
   },
   {
+    id: 4,
     title: "超級英雄",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et beatae eaque odio suscipit eos consectetur, molestiae unde totam a sint cumque libero? Autem quae laboriosam enim similique exercitationem",
   },
   {
+    id: 5,
     title: "科幻",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et beatae eaque odio suscipit eos consectetur, molestiae unde totam a sint cumque libero? Autem quae laboriosam enim similique exercitationem ",
   },
-  {
-    title: "家庭",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et beatae eaque odio suscipit eos consectetur, molestiae unde totam a sint cumque libero? Autem quae laboriosam enim similique exercitationem e",
+];
+// ============DATA============
+const pageData = reactive({
+  editItemList: [],
+  displayItemList: [],
+  inputData: {
+    id: 0,
+    title: "",
+    content: "",
   },
-]);
-const pageStatus = reactive({
-  mode: "normal", // normal, editing, creating
 });
 
-function goToEditing(key) {
-  alert(
-    `我要準備編輯第${key + 1}個成員了,它的標題是：${todoListItemsData[key].title}`,
-  );
+// 初始化資料
+pageData.editItemList = todoListItemsData.map((item) => {
+  return {
+    ...item,
+    isDisplay: false,
+    isClick: false,
+  };
+});
 
-  pageStatus.mode = "editing";
-}
+// ============Status============
+const pageStatus = reactive({
+  mode: "normal", //normal,editing,creating
+  showUpCreatingMode: computed(() => {
+    if (pageStatus.mode === "normal") return "";
+    else if (pageStatus.mode === "editing") return ":編輯模式";
+    else return ":新增模式";
+  }),
+});
+
+// ============functions============
+const pageAction = reactive({
+  // 清空
+  clearCard() {
+    pageStatus.mode = "normal";
+    pageData.inputData.id = 0;
+    pageData.inputData.title = "";
+    pageData.inputData.content = "";
+  },
+  //   編輯模式資料
+  goToEditing: (key) => {
+    const selectedCard = pageData.editItemList[key];
+    // TODO  why!!??
+    // pageData.inputData.id = selectedCard.id;
+    // pageData.inputData.title = selectedCard.title;
+    // pageData.inputData.content = selectedCard.content;
+    pageData.inputData = { ...selectedCard };
+
+    pageStatus.mode = "editing";
+  },
+  // 確定編輯/新增判斷
+  finishEditingOrCreating() {
+    if (pageStatus.mode === "creating") {
+      pageData.editItemList.push({
+        // TODO +1 ?? why!!??
+        id: pageData.editItemList.length + 1,
+        title: pageData.inputData.title,
+        content: pageData.inputData.content,
+      });
+    } else if (pageStatus.mode === "editing") {
+      const index = pageData.editItemList.findIndex(
+        (item) => item.id === pageData.inputData.id,
+      );
+      pageData.editItemList[index] = {
+        ...pageData.inputData,
+      };
+    }
+    pageAction.clearCard();
+  },
+  // 刪除卡片
+  deleteCard(key) {
+    const confirmed = confirm("確定刪除?");
+    if (confirmed) {
+      pageData.editItemList.splice(key, 1);
+    }
+  },
+  // 更新顯示區
+  renewDisplayItemList() {
+    // TODO  why!!?? 更新上傳條件
+    // 擴充todoListItemsData為formatShowListItemData
+    let formattedEditItemList = pageData.editItemList.map((item) => {
+      return { ...item, isDisplay: true };
+    });
+
+    formattedEditItemList = formattedEditItemList.filter((item) => {
+      return item.isClick == true;
+    });
+
+    pageData.displayItemList.splice(
+      0,
+      pageData.displayItemList.length,
+      ...formattedEditItemList,
+    );
+  },
+  // TODO  why!!??why!!??why!!??why!!??why!!??
+  selectAll: () => {
+    pageData.editItemList = pageData.editItemList.map((item) => {
+      return {
+        ...item,
+        isClick: true,
+      };
+    });
+  },
+  selectReAll: () => {
+    pageData.editItemList = pageData.editItemList.map((item) => {
+      return {
+        ...item,
+        isClick: !item.isClick,
+      };
+    });
+  },
+});
 </script>
 
 <style scoped></style>
